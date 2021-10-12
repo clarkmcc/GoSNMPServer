48be6374af390fbab5e18c13806556bdfdd962f8
Clark McCauley <clark.mccauley@printtrackerpro.com>
2021-10-12 14:55:45 -0600

    Fixed gosnmp import and resolved the new incompatibilities

--- mibImps/mibImpserve_test.go
+++ mibImps/mibImpserve_test.go
@@ -2,8 +2,8 @@ package mibImps
 
 import (
 	"bytes"
+	"github.com/gosnmp/gosnmp"
 	"github.com/sirupsen/logrus"
-	"github.com/slayercat/gosnmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"net"
